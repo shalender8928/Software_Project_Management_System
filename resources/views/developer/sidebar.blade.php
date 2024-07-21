02abeea8175ad18e1942045690ff009c0bb94336
Your Name <your.email@example.com>
2024-07-21 10:57:28 -0900

    project manager

--- resources/views/developer/sidebar.blade.php
+++ resources/views/developer/sidebar.blade.php
@@ -1,11 +1,7 @@
 <nav class="sidebar">
 <div class="sidebar-header">
 <a href="#" class="sidebar-brand" style="font-size:5;">
-<<<<<<< HEAD
 Developer<span> spms</span>
-=======
-  seniorManager<span> spms</span>
->>>>>>> df9f9bcbcaa6c9db8ceb3888cbb367345557baee
 </a>
 <div class="sidebar-toggler not-active">
   <span></span>
@@ -18,11 +14,7 @@ Developer<span> spms</span>
 <ul class="nav">
   <li class="nav-item nav-category">Main</li>
   <li class="nav-item">
-<<<<<<< HEAD
     <a href="{{route('developer.dashboard')}}" class="nav-link">
-=======
-    <a href="{{route('projectManager.dashboard')}}" class="nav-link">
->>>>>>> df9f9bcbcaa6c9db8ceb3888cbb367345557baee
       <i class="link-icon" data-feather="box"></i>
       <span class="link-title">Dashboard</span>
     </a>
@@ -30,7 +22,6 @@ Developer<span> spms</span>
 
   <li class="nav-item nav-category">home</li>
 
-<<<<<<< HEAD
       <!-- start the manager project task-->
 
   <li class="nav-item">
@@ -42,7 +33,7 @@ Developer<span> spms</span>
     <div class="collapse" id="emails">
       <ul class="nav sub-menu">
         <li class="nav-item">
-        <a class="nav-link" href="{{url('create_project')}}">iew assigned task</a>
+        <a class="nav-link" href="{{url('create_project')}}">View assigned task</a>
         
         </li>
         
@@ -77,41 +68,6 @@ Developer<span> spms</span>
 
 
 
-=======
- <!-- start the manage Development team -->
-
-  <li class="nav-item">
-      <ul class="nav sub-menu">
-      <li class="nav-item">
-          <a href="pages/email/inbox.html" class="nav-link">notify issues</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/inbox.html" class="nav-link">view list of project manager</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/read.html" class="nav-link">view list of all project</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/compose.html" class="nav-link">view list of development team</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/compose.html" class="nav-link">approve project plan</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/compose.html" class="nav-link">cancel project</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/inbox.html" class="nav-link">view  report</a>
-        </li>
-        <li class="nav-item">
-          <a href="pages/email/compose.html" class="nav-link">view feedback</a>
-        </li>
-      </ul>
-    
-  </li>
-
-
->>>>>>> df9f9bcbcaa6c9db8ceb3888cbb367345557baee
 </ul>
 </div>
 </nav>
