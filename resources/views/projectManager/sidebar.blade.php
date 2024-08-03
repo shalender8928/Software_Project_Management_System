206e5991b5dc43eef9ca94c85505ca864c46b609
Your Name <your.email@example.com>
2024-08-02 21:18:05 -0900

    new project manager

--- resources/views/projectManager/sidebar.blade.php
+++ resources/views/projectManager/sidebar.blade.php
@@ -22,78 +22,7 @@
 
       <li class="nav-item nav-category">home</li>
 
-      <!-- Manager Project Task -->
-      <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#project" role="button" aria-expanded="false" aria-controls="project">
-          <i class="link-icon" data-feather="folder-plus"></i>
-          <span class="link-title">Project</span>
-          <i class="link-arrow" data-feather="chevron-down"></i>
-        </a>
-        <div class="collapse" id="project">
-          <ul class="nav sub-menu">
-            <li class="nav-item">
-              <a href="{{url('Create_project')}}" class="nav-link">Create New Project</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ url('view_project_list') }}" class="nav-link">View Project List</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ route('projectmanager.edit_project') }}" class="nav-link">Edit Project</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{route('projectmanager.delete_project')}}" class="nav-link">Delete Project</a>
-            </li>
-            <li class="nav-item">
-              <a href="pages/email/compose.html" class="nav-link">Track Project Progress</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#project-plan" role="button" aria-expanded="false" aria-controls="project-plan">
-          <i class="link-icon" data-feather="folder-plus"></i>
-          <span class="link-title">Project Plan</span>
-          <i class="link-arrow" data-feather="chevron-down"></i>
-        </a>
-        <div class="collapse" id="project-plan">
-        <ul class="nav sub-menu">
-    <li class="nav-item">
-        <a href="{{ route('projectmanager.create_project_plan') }}" class="nav-link">Create Project Plan</a>
-    </li>
-  
-    <li class="nav-item">
-        <a href="{{ route('projectmanager.update_ProjectPlan') }}" class="nav-link">Update Plan</a>
-    </li>
-
-</ul>
-        </div>
-      </li>
-
-      <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#task" role="button" aria-expanded="false" aria-controls="task">
-          <i class="link-icon" data-feather="folder-plus"></i>
-          <span class="link-title">Task</span>
-          <i class="link-arrow" data-feather="chevron-down"></i>
-        </a>
-        <div class="collapse" id="task">
-          <ul class="nav sub-menu">
-            <li class="nav-item">
-              <a href="{{ route('ProjectManager.Assigntask') }}" class="nav-link">Create Assign Task</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ route('ProjectManager.Edit_Assigntask') }}" class="nav-link">Edit Assign Task</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ route('projectmanager.delete_task')}}" class="nav-link">Delete Task</a>
-            </li>
-            <li class="nav-item">
-              <a href="{{ url('view_task_list') }}" class="nav-link">View Task List</a>
-            </li>
-          </ul>
-        </div>
-      </li>
-
+      
       <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#milestone" role="button" aria-expanded="false" aria-controls="milestone">
           <i class="link-icon" data-feather="folder-plus"></i>
